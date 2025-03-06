eab589e5588b6b3d31f1682eba693b9ef9d9a521
ruthwik-t <ruthwik2525@gmail.com>
2025-03-06 06:15:03 +0530

    asdasd

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ app.use(express.json());
 
 // GET request
 app.get('/get', (req, res) => {
-  res.send('GET request received');
+  res.json('GET request received');
 });
 
 // POST request
 app.post('/post', (req, res) => {
-  res.send('POST request received');
+  res.json('POST request received');
 });
 
 // PUT request
 app.put('/put', (req, res) => {
-  res.send('PUT request received');
+  res.json('PUT request received');
 });
 
 // DELETE request
 app.delete('/delete', (req, res) => {
-  res.send('DELETE request received');
+  res.json('DELETE request received');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running`);
 });
